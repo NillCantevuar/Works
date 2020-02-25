c6718312785f3d1cba800ad37e2b9d71aef4a599
Bartosz Niziol <vxcx940@gmail.com>
2020-02-25 17:44:04 +0100

    Menu Partt

---
+++ src/main/java/whole/storage/exercise/ExerciseStorage.java
@@ -0,0 +1,8 @@
+package whole.storage.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseStorage {
+    public static List<Exercise> exercisesList = new ArrayList<>();
+}
